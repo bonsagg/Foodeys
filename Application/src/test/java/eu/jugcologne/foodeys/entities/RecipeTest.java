46adc233ee94eecbb5997ac57324c2a3b48b230e
Sergey Shcherbakov <sshcherbakov@vmware.com>
2013-03-18 00:27:39 +0100

    Added Arquillian integration test support

---
+++ Application/src/test/java/eu/jugcologne/foodeys/entities/RecipeTest.java
@@ -0,0 +1,35 @@
+/**
+ * 
+ */
+package eu.jugcologne.foodeys.entities;
+
+import static org.junit.Assert.fail;
+
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.asset.EmptyAsset;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ * @author "Sergey Shcherbakov"
+ *
+ */
+@RunWith(Arquillian.class)
+public class RecipeTest {
+
+	@Deployment
+    public static JavaArchive createDeployment() {
+        return ShrinkWrap.create(JavaArchive.class)
+            .addClass(Recipe.class)
+            .addAsManifestResource(EmptyAsset.INSTANCE, "beans.xml");
+    }
+
+	@Test
+	public void test() {
+		//fail("Not yet implemented");
+	}
+
+}
