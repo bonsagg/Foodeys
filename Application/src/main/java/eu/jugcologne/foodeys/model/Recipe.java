ad906469cd07ee6210c77d9b1e04e2bbc9b38c57
muellermi <michael.mueller@mueller-bruehl.de>
2013-03-25 22:26:17 +0100

    example of a simple recipe

--- Application/src/main/java/eu/jugcologne/foodeys/model/Recipe.java
+++ Application/src/main/java/eu/jugcologne/foodeys/model/Recipe.java
@@ -15,6 +15,33 @@ import javax.persistence.Entity;
  * For general, any recipe is build upon a list of parts. 
  * A simple recipe contains just one part.
  * Other attributes: portions count, nutrition facts, picture(s)
+ * 
+ * Sample:
+ * 
+ * Michael's pan cake (2 pieces)
+ * 
+ * Preparation time: 5 min
+ * Cooking time: 5 min
+ * 
+ * Ingredients
+ * 
+ * 2 tablespoon (heapened) of flour
+ * 150 ml (apx.) milk
+ * 1 egg
+ * Toppings of choice
+ * 
+ * Directions
+ * 1. Mix flour, milk and egg, giving a fluid paste
+ * 2. Flow half into a big pan (28-30 cm)
+ * 3. When the upper side gets tight, turn it arround
+ * 4. For a tasty pan cake, add topping like cheese and / or ham
+ * 5. Continue baking until both sides get golden-brown
+ * 6. Prepare second pan cake
+ * 7. For a sweet version, serve with marmelade, otherwise with warm tasty topping
+ * 
+ * This is not a real good recipe, but it shows some aspects of a simple recipe.
+ * 
+ * A more complex recipe may be composed of more parts or ohter recipes.
  */
 @Entity
 public class Recipe extends AbstractEntity {
