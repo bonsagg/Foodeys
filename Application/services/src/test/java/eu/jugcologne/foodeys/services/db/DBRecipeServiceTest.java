22c6b224cfe527c28ef9454013bb48213749c5d8
Daniel Sachse <sachsedaniel@gmail.com>
2013-09-07 15:23:08 +0200

    Added DBRecipeServiceTest

--- Application/services/src/test/java/eu/jugcologne/foodeys/services/db/DBRecipeServiceTest.java
+++ Application/services/src/test/java/eu/jugcologne/foodeys/services/db/DBRecipeServiceTest.java
@@ -1,9 +1,14 @@
 package eu.jugcologne.foodeys.services.db;
 
 import eu.jugcologne.foodeys.FoodeysMarker;
+import eu.jugcologne.foodeys.persistence.model.Food;
+import eu.jugcologne.foodeys.persistence.model.Recipe;
+import eu.jugcologne.foodeys.services.api.FoodService;
 import eu.jugcologne.foodeys.services.api.RecipeService;
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.arquillian.junit.InSequence;
+import org.jboss.arquillian.persistence.UsingDataSet;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.asset.EmptyAsset;
 import org.jboss.shrinkwrap.api.spec.WebArchive;
@@ -12,11 +17,18 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import javax.inject.Inject;
+import java.util.List;
 
 @RunWith(Arquillian.class)
 public class DBRecipeServiceTest {
+    public static final String tomatoSoupName = "Tomato Soup";
+    public static final String tomatoSoupInstructions = "Cook tomatoes, add salt and pepper";
+
     @Inject
     private RecipeService recipeService;
+
+    @Inject
+    private FoodService foodService;
 
     @Deployment
     public static WebArchive createDeployment() {
@@ -28,7 +40,59 @@ public class DBRecipeServiceTest {
     }
 
     @Test
+    @InSequence(1)
     public void testIsDeployed() {
         Assert.assertNotNull(recipeService);
+    }
+
+    @Test
+    @InSequence(2)
+    @UsingDataSet("datasets/DBRecipeServiceTest.yml")
+    public void testFindByID() {
+        Recipe tomatoSoup = recipeService.findByID(1l);
+
+        Assert.assertNotNull(tomatoSoup);
+        Assert.assertEquals(tomatoSoupName, tomatoSoup.getName());
+        Assert.assertEquals(3, tomatoSoup.getIngredients().size());
+    }
+
+    @Test
+    @InSequence(3)
+    @UsingDataSet("datasets/DBRecipeServiceTest.yml")
+    public void testFindAllRecipes() {
+        List<Recipe> recipes =  recipeService.findAllRecipes();
+
+        Assert.assertEquals(1, recipes.size());
+
+        Recipe tomatoSoup = recipes.get(0);
+
+        Assert.assertEquals(tomatoSoupName, tomatoSoup.getName());
+        Assert.assertEquals(tomatoSoupInstructions, tomatoSoup.getInstructions());
+        Assert.assertEquals(3, tomatoSoup.getIngredients().size());
+    }
+
+    @Test
+    @InSequence(4)
+    @UsingDataSet("datasets/DBRecipeServiceTest.yml")
+    public void testFindAllRecipesForFood() {
+        Food tomato = foodService.findByID(1l);
+        Food salt = foodService.findByID(2l);
+        Food pepper = foodService.findByID(3l);
+
+        List<Recipe> tomatoRecipes = recipeService.findAllRecipesForFood(tomato);
+        List<Recipe> saltRecipes = recipeService.findAllRecipesForFood(salt);
+        List<Recipe> pepperRecipes = recipeService.findAllRecipesForFood(pepper);
+
+        Assert.assertEquals(1, tomatoRecipes.size());
+        Assert.assertEquals(1, saltRecipes.size());
+        Assert.assertEquals(1, pepperRecipes.size());
+
+        Recipe tomatoSoup1 = tomatoRecipes.get(0);
+        Recipe tomatoSoup2 = saltRecipes.get(0);
+        Recipe tomatoSoup3 = pepperRecipes.get(0);
+
+        Assert.assertEquals(tomatoSoup2, tomatoSoup1);
+        Assert.assertEquals(tomatoSoup3, tomatoSoup2);
+        Assert.assertEquals(tomatoSoup1, tomatoSoup3);
     }
 }
